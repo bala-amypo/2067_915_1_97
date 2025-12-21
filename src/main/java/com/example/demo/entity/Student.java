ce9d9e1fca55d9d44dc5ab3798a7f5175f9e3564
bala-amypo <bala@amypo.in>
2025-12-21 17:16:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
@@ -10,12 +11,24 @@ public class Student {
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
+
+    @Column(unique = true)
     private String rollNumber;
 
     public Student() {}
 
+    public Student(Long id, String name, String email, String rollNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.rollNumber = rollNumber;
+    }
+
     public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
