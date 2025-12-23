f6ef68165a27e43141e748e12dbe0c734dfee66f
bala-amypo <bala@amypo.in>
2025-12-23 04:50:46 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,7 +1,38 @@
-package com.example.demo.exception;
+package com.example.demo.service.impl;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.example.demo.entity.Certificate;
+import com.example.demo.entity.VerificationLog;
+import com.example.demo.repository.CertificateRepository;
+import com.example.demo.repository.VerificationLogRepository;
+import com.example.demo.service.VerificationService;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+import java.time.LocalDateTime;
+
+public class VerificationServiceImpl implements VerificationService {
+
+    private final CertificateRepository certRepo;
+    private final VerificationLogRepository logRepo;
+
+    public VerificationServiceImpl(
+            CertificateRepository certRepo,
+            VerificationLogRepository logRepo
+    ) {
+        this.certRepo = certRepo;
+        this.logRepo = logRepo;
+    }
+
+    @Override
+    public VerificationLog verifyCertificate(String code, String ip) {
+
+        Certificate cert = certRepo.findByVerificationCode(code).orElse(null);
+
+        VerificationLog log = new VerificationLog(
+                cert,
+                code,
+                ip,
+                LocalDateTime.now()
+        );
+
+        return logRepo.save(log);
+    }
 }
