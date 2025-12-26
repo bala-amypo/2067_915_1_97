f17af503b4cec61e754311819969ef5711153d63
bala-amypo <bala@amypo.in>
2025-12-26 08:44:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VerificationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VerificationServiceImpl.java
@@ -1,35 +1,22 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.Certificate;
 import com.example.demo.entity.VerificationLog;
-import com.example.demo.repository.CertificateRepository;
 import com.example.demo.repository.VerificationLogRepository;
 import com.example.demo.service.VerificationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
 public class VerificationServiceImpl implements VerificationService {
-    private final CertificateRepository certificateRepository;
-    private final VerificationLogRepository logRepository;
 
-    public VerificationServiceImpl(CertificateRepository cr, VerificationLogRepository lr) {
-        this.certificateRepository = cr;
-        this.logRepository = lr;
-    }
+    @Autowired
+    private VerificationLogRepository repository;
 
     @Override
-    public VerificationLog verifyCertificate(String code, String ip) {
-        Optional<Certificate> cert = certificateRepository.findByVerificationCode(code);
-        
-        VerificationLog log = VerificationLog.builder()
-                .certificate(cert.orElse(null))
-                .verifiedAt(LocalDateTime.now())
-                .ipAddress(ip)
-                .status(cert.isPresent() ? "SUCCESS" : "FAILED")
-                .build();
-        
-        return logRepository.save(log);
+    public VerificationLog verify() {
+        VerificationLog log = new VerificationLog();
+        log.setStatus("VERIFIED");
+        return repository.save(log);
     }
 }
